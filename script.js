507a7793dafad61fbbbf0fe35fc27793d993a8fc
Prashant Pal <palprashant465@gmail.com>
2025-02-28 22:42:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toCase(text) {
+    
+    if(text.length==0) return '-';
     text = text.replace(/[^a-zA-Z0-9]/g, '');
-    let fin = `${text.toLowerCase()}- ${text.toUpperCase()}`
+    let fin = `${text.toLowerCase()}-${text.toUpperCase()}`
     return fin;
 }
 
